f096b9c84c7a916fd593f5efdd6514169e7482cf
지태 <wlxo402@naver.com>
2024-09-05 01:14:45 +0900

    Feat Paginate

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NetFlixLogo from "../images/netflixText.png";
+import { Link } from "react-router-dom";
 
 export default function AppLayout() {
+  const [keyword, setKeyword] = useState("");
+
+  const navigate = useNavigate();
+
+  const searchByKeyword = (event) => {
+    event.preventDefault();
+    navigate(`/movies?q=${keyword}`);
+    setKeyword("");
+  };
   return (
     <div>
       <Navbar expand="lg" className="px-3 bg-black" data-bs-theme="dark">
@@ -30,36 +40,42 @@ export default function AppLayout() {
               navbarScroll
             >
               <Nav
-                // as={Link}
+                as={Link}
                 to="/"
                 style={{
                   color: "white",
                   fontWeight: "bold",
                   fontSize: "1.2rem",
+                  textDecoration: "none",
                 }}
               >
                 Home
               </Nav>
               <Nav
-                // as={Link}
+                as={Link}
                 to="/movies"
                 style={{
                   color: "white",
                   fontWeight: "bold",
                   fontSize: "1.2rem",
+                  textDecoration: "none",
                 }}
               >
                 Movies
               </Nav>
             </Nav>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={(e) => searchByKeyword(e)}>
               <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
               />
-              <Button variant="danger">Search</Button>
+              <Button variant="danger" type="submit">
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,5 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
+import "./MoviePage.style.css";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Alert, Spinner, Container, Row, Col } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
+import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
 
 export default function MoviePage() {
-  return <div>MoviePage</div>;
+  const [page, setPage] = useState(1);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const { data: genreData } = useMovieGenreQuery();
+
+  const handleGenreChange = (genreId) => {
+    setSelectedGenres((prevSelectedGenres) => {
+      const isGenreSelected = prevSelectedGenres.includes(genreId);
+
+      if (isGenreSelected) {
+        return prevSelectedGenres.filter((id) => id !== genreId);
+      } else {
+        return [...prevSelectedGenres, genreId];
+      }
+    });
+  };
+
+  const [query] = useSearchParams();
+  const keyword = query.get("q");
+
+  const handlePageClick = (page) => {
+    setPage(page.selected + 1);
+  };
+
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="spinner-area">
+        <Spinner animation="border" variant="light" />
+      </div>
+    );
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+  if (!(data && data.results && data.results.length !== 0)) {
+    return <div className="noData">No data available</div>;
+  }
+  console.log(data);
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          {/*필터 */}
+          <Col lg={4} xs={12}>
+            {genreData?.map((genre) => (
+              <div className="checkbox-container" key={genre.id}>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  id={`genre-${genre.id}`}
+                  checked={selectedGenres.includes(genre.id)}
+                  onChange={() => handleGenreChange(genre.id)}
+                />
+                <label className="checkbox-label" htmlFor={`genre-${genre.id}`}>
+                  {genre.name}
+                </label>
+              </div>
+            ))}
+          </Col>
+          {/*영화 카드 */}
+          <Col lg={8} xs={12}>
+            <Row>
+              {data?.results
+                .filter((movie) => movie.poster_path)
+                .filter((movie) => {
+                  return (
+                    selectedGenres.length === 0 ||
+                    movie.genre_ids.some((genreId) =>
+                      selectedGenres.includes(genreId)
+                    )
+                  );
+                })
+
+                .map((movie) => {
+                  return (
+                    <Col key={movie.id} lg={4} xs={12}>
+                      <MovieCard movie={movie} />
+                    </Col>
+                  );
+                })}
+
+              {/* {data?.results
+                .filter((movie) =>
+                  movie.genre_ids.some((genreId) =>
+                    selectedGenres.includes(genreId)
+                  )
+                )
+                .map((movie) => (
+                  <Col key={movie.id} lg={4} xs={12}>
+                    <MovieCard movie={movie} />
+                  </Col>
+                ))} */}
+            </Row>
+          </Col>
+
+          <ReactPaginate
+            nextLabel="next"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={10}
+            marginPagesDisplayed={2}
+            pageCount={data.total_pages}
+            previousLabel="previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            forcePage={page - 1}
+          />
+        </Row>
+      </Container>
+    </div>
+  );
 }
